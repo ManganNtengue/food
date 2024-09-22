9c4551bef1aa8da2934bbbb912f128a929b9677e
ManganNtengue <mangankevin28@yahoo.com>
2024-09-22 17:37:57 +0100

    FO2:ErrorPage>creation

---
+++ src/components/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div>
+      <h1>ErrorPage</h1>
+      <button>Retourner à la page d'accueil</button>
+    </div>
+  );
+}
+
+export default ErrorPage;
